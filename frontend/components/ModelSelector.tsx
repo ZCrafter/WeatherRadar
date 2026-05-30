cc5ae7806834052f33687b8d7fb3f48d8dcfeb36
ZCrafter <zach510harrison@gmail.com>
2026-05-30 10:17:39 -0400

    Create ModelSelector.tsx

---
+++ frontend/components/ModelSelector.tsx
@@ -0,0 +1,18 @@
+export default function ModelSelector({
+  models,
+}: {
+  models: any[];
+}) {
+  return (
+    <div style={{ marginBottom: 24 }}>
+      <h2>Models</h2>
+      <ul>
+        {models.map((m) => (
+          <li key={m.name}>
+            {m.name} {m.enabled ? "enabled" : "disabled"}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
